72900d1fca6f2db22f46a9dbfb1e92acf44aeefd
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-14 09:02:27 -0800

    semicolons

--- src/project.module.js
+++ src/project.module.js
@@ -1,39 +1,40 @@
 export default class Project {
     constructor(name) {
-        this.name = name
-        this.todos = []
+        this.name = name;
+        this.todos = [];
     }
+
     getName() {
-        return this.name
+        return this.name;
     }
 
     getTodos() {
-        return [...this.todos]
+        return [...this.todos];
     }
 
     getTodo(index) {
-        return this.todos[index]
+        return this.todos[index];
     }
 
     setTodos(value) {
-        this.todos = value
+        this.todos = value;
     }
 
     addTodo(todo) {
-        const newTodos = this.getTodos()
-        newTodos.push(todo)
-        this.setTodos(newTodos)
+        const newTodos = this.getTodos();
+        newTodos.push(todo);
+        this.setTodos(newTodos);
     }
 
     removeTodo(index) {
-        const newTodos = this.getTodos()
-        newTodos.splice(index,1)
-        this.setTodos(newTodos)
+        const newTodos = this.getTodos();
+        newTodos.splice(index,1);
+        this.setTodos(newTodos);
     }
 
     updateTodo(index, todo) {
-        const newTodos = this.getTodos()
-        newTodos[index] = todo
-        this.setTodos(newTodos)
+        const newTodos = this.getTodos();
+        newTodos[index] = todo;
+        this.setTodos(newTodos);
     }
 }
