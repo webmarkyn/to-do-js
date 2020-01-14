02bb9f430dbdcd94578e476e1f96ce0b692ca529
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-14 08:55:47 -0800

    scripts added to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
+const path = require('path');
+
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'main.js'
     }
-}
+};
